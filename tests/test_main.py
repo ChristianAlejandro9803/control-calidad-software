a0d67c0ab884f3fd5ff0392175261b4c7b964c03
Christian Alejandro <christianalejandro9803@gmail.com>
2025-04-30 04:52:01 -0700

    Agrega función ejemplo y corrige pruebas para CI

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-# Agrega la carpeta raíz del proyecto al path de Python
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Añadir la carpeta 'app' al path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
 
-from main import saludar
+from saludo import saludar
 
 def test_saludo():
     assert saludar() == "Hola mundo"
